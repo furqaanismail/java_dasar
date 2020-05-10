06d7d50932c62bbe69e0374f312b9f003161ce13
furqan <ismailfurqaan.com>
2020-05-10 13:52:43 +0800

    first commit

---
+++ src/latihan1/Hewan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan1;
+
+/**
+ *
+ * @author FRQ
+ */
+public class Hewan {
+    private String namahewan="ayam";
+    public String hewana;
+
+    public void setNamahewan(String namahewan) {
+        this.namahewan = namahewan;
+    }
+
+    
+
+    public String getNamahewan() {
+        return namahewan;
+    }
+    
+    
+    public void suara(){
+        System.out.println("sttt");
+        System.out.println("");
+    }
+    
+   
+}
---
+++ src/latihan1/Main.java
@@ -0,0 +1,39 @@
+
+package latihan1;
+
+public class Main extends Hewan{
+    public Main(){
+        
+    }
+    public Main(int g){
+        
+    }
+    public String tambah(){
+        return "aa";
+    }
+    public String tambah(int a){
+        return "bb";
+    }
+    //enkapsulasi
+    //pembungkusan
+    //inheritance
+    //pewarisan
+    //polimorfisme
+    //banyak bentuk
+    public static void main(String[] args) {
+        int a=5;
+        String nama;
+        
+        if(a<8){
+            
+        }else{
+            
+        }
+        
+         Main m1 = new Main();
+        m1.suara();
+        
+        m1.setNamahewan("harimau");
+        System.out.println(m1.getNamahewan());
+    }
+}
